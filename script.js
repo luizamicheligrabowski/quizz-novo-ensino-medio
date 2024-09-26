88b7c2e2508131a0fd42e6b40b4a66ae8e52014a
luizamicheligrabowski <106758992+luizamicheligrabowski@users.noreply.github.com>
2024-09-26 09:08:29 -0300

    pronto

--- script.js
+++ script.js
@@ -5,72 +5,53 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
+    
+        enunciado: "o novo ensino médio é uma reforma educacional com o aumento da carga horaria de 800h para 1000h. Também foi adicionado além das disciplinas obrigatórias como portuguẽs e matemática, os estudantes também podem escolher a trilha de aprendizagem que desejam seguir a partir do segundo ano, por meio dos itinerários formativos. O objetivo é modernizar a educação e adaptar os jovens para o mercado de trabalho.Vocẽ acha que essas mudanças são benéficas ou maléficas para os estudantes?
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Isso é benéfico. Visto que o estudante poderá escolher em que área quer se aprofundar e então, se preparar melhor para o mercado de trabalho.",
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Isso é maléfico. Logo que o curriculo é muito rígido e não flexivel, então pode se arrepender de qual trilha resolvel escolher."",
+            }
+        ]
+
+{
+            
+        enunciado: "O estudante terá maior possibilidade de escolha: A idéia era trazer uma maior variedade de opções porém,é necessario uma maior autoconciencia já no primeiro ano do ensino médio tendo em mente qual profissão deseja seguir para não errar na escolha. Você acha que um maior leque de possibilidades ajuda ou atrapalha a escolha do estudante ?"
+        alternativas: [
+            
+                texto: "Ajuda. Visto que é uma preparaçao para o mercado de trabalho , pois já terá um, maior conhecimento na aréa que irá futuramente trabalhar."
+        
+            {
+                texto: "Atrapalha. Logo que gera uma pressão no estudante que ao escolher sua carreira podem mudar de idéia e então ter perdido tempo de estudo.",
             }
         ]
     },
-    {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+{
+        enunciado: "o novo ensino médio e o ENEM: apesarde ter muitas possibilidades de escolha dos itinerários, uma preocupação copm relação a preparação dos alunos para o Exame Nacional do Ensino Médio, que atualmente é a maior porta de entrada para o ensino superior. Como você classifíca esse impacto?",
         alternativas: [
+            
+                texto: "Positivo. Trás uma profundidade maior em conhecimentos expecíficos, que podem ajudar na hora de fazer a  prova.",
+                
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
-            },
-            {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Negativo. Porque gera uma lacuna onde deixa a desejar muito em alguns aspectos, o que será prejudicial ao estudantes.",
+                
             }
         ]
     },
-    {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+    {  
+        enunciado: "As plataformas: Uma das principais mudanças do novo ensino médio é a implementação de plataformas digitais que vizam melhorar a aprendizagem. porém nem tudos tem um bom acesso a tecnologia. Você considera as plataformas um bom método de ensino ?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Sim. Os alunos passam a ter mais possibilidades de acesso a educação forado ambiente escolar.",
+               
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Não. Já que nem todos tem o mesmo acesso a internet alguns alunos podem ser prejudicados, além de que o desempenho cai sem o auxilio de um professor para orientar os alunos!",
+               
             }
         ]
     },
-    {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
-        alternativas: [
-            {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
-            },
-            {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
-            }
-        ]
-    },
-    {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
-        alternativas: [
-            {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
-            },
-            {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
-            }
-        ]
-    },
-];
 
 
 let atual = 0;
@@ -105,7 +86,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "O novo ensino médio é uma tentativa de modernizar a educação, buscando preparar o jovem para o mercado de trabalho, primorando suas competências e o melhor desenvolvimento dos jovens. Existem vantágens e pontos positivos, como a possibilidade de escolha autonoma de qual rumo profissional o jovem quer seguir. Há um grande potêncial de melhoria na educação brasileira, poŕem necessitam de investimentosem treinamento e capacitação dos professores, já que nem todas as escolas possuem infraestrutura para receber essas mudanças. Para obt4er exito nessa reforma,ainda exige muitos esforço por parte dos alunos e educadores, o que acarreta em um furo na educação e no conhecimento dos jovens. ";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
